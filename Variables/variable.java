483f6270cd347475275ab31e0c84ad749d802a7c
nashitshahi17 <nashitshahi17@gmail.com>
2026-07-09 23:06:27 +0530

    variables

---
+++ Variables/variable.java
@@ -0,0 +1,9 @@
+public class variable {
+    public static void main(String[] args) {
+        int num = 1;
+        int _num = 2;
+        int result = num + _num;
+        System.out.println(result);
+    }
+}
+// Variables are containers to store the values 
