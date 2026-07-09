ef32f0630bcecf1e94db0a112e50c4119d037add
nashitshahi17 <nashitshahi17@gmail.com>
2026-07-09 23:23:12 +0530

    different data types

---
+++ Variables/var3.java
@@ -0,0 +1,14 @@
+public class var3 {
+    public static void main(String[] args) {
+        char c = 'M'; // Chars is of 2 bytes in java and it always made with single quotes double quotes is for string.
+        System.out.println(c);
+        boolean a = true; // in java for boolean we only have true or false not 0 or 1
+        System.out.println(a);
+
+        // byte b = 129; it will give error becuase byte is from -127 to 127
+        byte b = 127;
+        short sh = 587;
+        long l = 5845l; // to make it identified as long we have to explicitly mention l at the end
+
+    }
+}
