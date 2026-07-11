0e2192ed60ca0dc9b532b5e5257364d170470418
nashitshahi17 <nashitshahi17@gmail.com>
2026-07-11 23:00:56 +0530

    take string input

--- Basics/Hello2.java
+++ Basics/Hello2.java
@@ -3,6 +3,9 @@ public class Hello2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
+        sc.nextLine(); // When nextInt() reads the integer, it does not consume the newline (\n) that you type after the number. So nextLine() immediately reads that leftover newline and returns an empty string.
+        String y = sc.nextLine();
         System.out.println(x);
+        System.out.println(y);
     }
 }
