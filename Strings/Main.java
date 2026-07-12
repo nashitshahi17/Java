34378a88f9420e58993b2fda08c5adf5c8957eab
nashitshahi17 <nashitshahi17@gmail.com>
2026-07-12 22:47:36 +0530

    reverse a string

--- Strings/Main.java
+++ Strings/Main.java
@@ -3,5 +3,10 @@ public class Main {
         String s = "Java";
         StringBuilder sb = new StringBuilder(s);
         System.out.println(sb.reverse().toString());
+        String s1 = "";
+        for(int i=0;i<s.length();i++){
+            s1 = s.charAt(i) + s1;
+        }
+        System.out.println(s1);
     }    
 }
